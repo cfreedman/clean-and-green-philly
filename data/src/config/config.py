f681fe2dd209f92578c82244da536365177230c7
Collum <collum.freedman@gmail.com>
2025-04-06 13:31:24 -0400

    fix some docker issues

--- data/src/config/config.py
+++ data/src/config/config.py
@@ -1,7 +1,7 @@
 import logging
 from pathlib import Path
 
-FORCE_RELOAD = False
+FORCE_RELOAD = True
 """ During the data load, whether to query the various GIS API services for the data to load into the postgres tables.  If True, will query the API services, backup the database, reload the database and report on data differences.  If false will read the data from postgres."""
 
 USE_CRS = "EPSG:2272"
